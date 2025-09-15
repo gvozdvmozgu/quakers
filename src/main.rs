2f6e538068b6e7735a11befc55700552c8c43fd1
Andrey Nikolaev <gvozdvmozgu@gmail.com>
2025-09-15 13:39:14 -0700

    Update QEMU executable name in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let bios_path = env!("BIOS_PATH");
-    let mut qemu = std::process::Command::new("qemu-system-x86_64.exe");
+    let mut qemu = std::process::Command::new("qemu-system-x86_64");
 
     qemu.arg("-drive")
         .arg(format!("format=raw,file={bios_path}"));
